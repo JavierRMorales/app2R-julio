80fb44df7752ebeef0f9321ebba5b9a8122e55f3
javier <javierr.morales39@gmail.com>
2019-07-04 12:28:20 -0500

    1

---
+++ js/acciones.js
@@ -0,0 +1,22 @@
+//JavaScript Document
+
+$(document).ready(function(e){
+	document.addEventListener("deviceready",function(){
+		
+$('#play').tap(function(){
+	audio
+});//tap reproducir
+
+$('#pause').tap(function(){
+	navigator.notification.beep(1000);
+});//tap pausar
+
+$('#stop').tap(function(){
+	navigator.notification.vibrate(1000);
+});//tap detener
+
+$('#buscar').click(function(){
+});//tap buscar
+
+	},false); //devicereafy
+});//ready 
